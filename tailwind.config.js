12b48b887d41b00d1cf9b291202dab175ad58f2c
Christian <christiandarlington0@gmail.com>
2023-05-18 14:05:42 -0400

    forms completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./Project3/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        custom: ['Noto Sans', 'sans-serf']
-      }
-    },
-  },
-  plugins: [],
-}
-
